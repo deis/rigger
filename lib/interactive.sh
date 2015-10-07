a48f986a2426451bbe0a1716fdb4365c9a999082
Seth Goings <sgoings@engineyard.com>
2015-10-07 14:35:48 -0700

    fix(prompt): sanitize output of prompt if in blacklist

--- lib/interactive.sh
+++ lib/interactive.sh
@@ -1,8 +1,28 @@
+function sanitize-variable {
+  # introduced for v1.11.0 secret leakage
+
+  local variable="${1}"
+  local force="${2:-false}"
+
+  local blacklist="AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+DO_TOKEN
+DO_SSH_FINGERPRINT"
+
+  if echo "${blacklist}" | grep -s "${variable}" &> /dev/null || \
+    [ ${force} == true ]; then
+    echo "******"
+  else
+    echo "${!variable}"
+  fi
+}
+
 function skip-prompt-if-set {
   local variable="${1}"
+  local sanitize="${2:-false}"
 
   if [ ! -z ${!variable:-} ]; then
-    rerun_log warn "${variable} already set to ${!variable}. Skipping prompt."
+    rerun_log warn "${variable} already set to $(sanitize-variable ${variable} ${sanitize}). Skipping prompt."
     return 1
   else
     return 0
@@ -49,6 +69,35 @@ function choice-prompt {
   done
 }
 
+function password-prompt {
+  local question="${1}"
+  local return_var="${2}"
+  local default="${3:-}"
+
+
+  skip-prompt-if-set "${return_var}" true || return 0
+
+  local input
+
+  while [ -z "${input:-}" ]; do
+
+    if [ -z ${default} ]; then
+      rerun_log "-> ${question} ${return_var} (no default)"
+    else
+      rerun_log "-> ${question} ${return_var} [ ${default:0:4}****** ]"
+    fi
+
+    read -s input
+
+    [ -n "${default}" ] && break
+
+  done
+
+  eval "export ${return_var}=${input:-${default}}"
+
+  echo "You chose: ${!return_var:0:4}******"
+}
+
 function prompt {
   local question="${1}"
   local return_var="${2}"
@@ -74,5 +123,5 @@ function prompt {
 
   eval "export ${return_var}=${input:-${default}}"
 
-  echo "You chose: ${!return_var}"
+  echo "You chose: $(sanitize-variable ${return_var})"
 }
